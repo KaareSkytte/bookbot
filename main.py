8029d242ca77fb9b77f19313267d04526a8cb928
KaareSkytte <Kaareskytte1431@gmail.com>
2025-02-16 11:46:23 +0100

    Count Characters

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 def main():    
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        return file_contents
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
-def num_words(text):
+
+def get_num_words(text):
     words = text.split()
-    print (f"{len(words)} words found in the document")
+    return len(words)
 
 
-num_words(main())
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
